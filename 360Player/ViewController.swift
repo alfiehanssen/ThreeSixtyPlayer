48ad73aa04c870847591062b6a30b455a9194acb
Alfie Hanssen <alfiehanssen@gmail.com>
2016-07-05 21:01:20 -0400

    Initial commit, Xcode project.

---
+++ 360Player/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  360Player
+//
+//  Created by Alfred Hanssen on 7/5/16.
+//  Copyright © 2016 Alfie Hanssen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
